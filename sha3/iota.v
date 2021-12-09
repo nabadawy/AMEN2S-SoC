cab75291b05b8844760521399132ba21e40a471a
Mariam Mousa <67393923+mariamhmousa@users.noreply.github.com>
2021-12-09 22:05:43 +0200

    Update iota.v

--- sha3/iota.v
+++ sha3/iota.v
@@ -1,7 +1,7 @@
 module iota (
-	input in_st_0,
-	input rndc_r,
-	output out_st_0
+  input [63:0] in_st_0,
+  input [63:0] rndc_r,
+  output [63:0] out_st_0
 );
 
 assign out_st_0 = in_st_0 ^ rndc_r;
