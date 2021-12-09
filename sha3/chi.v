00a7a383ce363207732a83274b344ff2590acebe
Mariam Mousa <67393923+mariamhmousa@users.noreply.github.com>
2021-12-09 21:41:47 +0200

    tested chi

--- sha3/chi.v
+++ sha3/chi.v
@@ -1,13 +1,12 @@
 module chi (
-    input [0:95] in_st, //takes three 32-bit blocks
-    output [0:31] out_st //outputs one 32-bit block
-    
+  input [191:0] in_st,
+  output [63:0] out_st     
 );
-    wire [0:31] bc[0:1];
+  wire [63:0] bc[0:1];
 
-    assign bc[0] = in_st[32:63];
-    assign bc[1] = in_st[64:95];
+  assign bc[0] = in_st[127:64];
+  assign bc[1] = in_st[191:128];
 
-    assign out_st = in_st[0:31] ^ (~bc[0] & bc[1]);
+  assign out_st = in_st[63:0] ^ (~bc[0] & bc[1]);
 
 endmodule
