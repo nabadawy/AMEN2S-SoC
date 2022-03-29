f5baa60cdf53045815c26843a0e62859201c0e90
eman3asem <62911896+eman3asem@users.noreply.github.com>
2022-03-29 10:15:00 +0200

    Delete ML Accelerator/1unit-accelerator directory

--- ML Accelerator/1unit-accelerator/flipFlop.v
+++
@@ -1,9 +0,0 @@
-module flipFlop#(
-  	parameter WIDTH = 8
-	)
-	(input clk, 
-	input [WIDTH-1:0] d, 
-	output reg [WIDTH-1:0] q);
-	always @ (posedge clk)
-		q <= d;                // pronounced “q gets d”
-endmodule
